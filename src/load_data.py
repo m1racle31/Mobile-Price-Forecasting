34bbc182d696b93a23cfafaca64b11b2548330a3
Yoshua Augusta <yoshuaaugusta31@gmail.com>
2024-09-16 12:55:45 +0700

    Updated data loading

---
+++ src/load_data.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+# Define the function to load data
+def read_csv_data(file_path):
+    # Load data from CSV file
+    df = pd.read_csv(file_path)
+
+    return df
+
+
+if __name__ == "__main__":
+    # Define the path to the dataset
+    file_path = "../data/Mobile Price Classification.csv"
+
+    # Load the data
+    df = read_csv_data(file_path)
+
+    # Display dataframe information
+    print("Dataframe Information:")
+    df.info()
+
+    # Display dataframe columns
+    print("\nDataframe Columns:")
+    print(df.columns)
